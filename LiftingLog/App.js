19847c283acd5c9ecd681776a13f996b38da2b09
DrakeH5 <68834232+DrakeH5@users.noreply.github.com>
2023-01-21 18:11:30 -0600

    big logo / setup project

---
+++ LiftingLog/App.js
@@ -0,0 +1,31 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import {Image} from 'react-native' ; 
+
+export default function App() {
+  const mainLogoStyle = {
+    width: "100%",
+    height: "100%",
+    resizeMode: 'stretch',
+    top: "-20%"
+  }
+  const viewStyle = {
+    backgroundColor: "rgb(10,10,10)"
+  }
+  return (
+    <View style={viewStyle}>
+      <Text>Lifting Log!</Text>
+      <Image style={mainLogoStyle} source={require('./mainLogo.png')} />
+      <StatusBar style="auto" /> 
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
