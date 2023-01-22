7926f61e2773aaeddb454a51cd146929dbd786b2
DrakeH5 <68834232+DrakeH5@users.noreply.github.com>
2023-01-21 18:27:16 -0600

    buttons

--- LiftingLog/App.js
+++ LiftingLog/App.js
@@ -1,22 +1,28 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import {Image} from 'react-native' ; 
+import {Image} from 'react-native'; 
+import {Button} from 'react-native'; 
 
 export default function App() {
   const mainLogoStyle = {
     width: "100%",
-    height: "100%",
+    height: "75%",
     resizeMode: 'stretch',
-    top: "-20%"
+    top: "-10%"
   }
   const viewStyle = {
-    backgroundColor: "rgb(10,10,10)"
+    backgroundColor: "rgb(10,10,10)",
+    height: "100%"
   }
   return (
     <View style={viewStyle}>
       <Text>Lifting Log!</Text>
       <Image style={mainLogoStyle} source={require('./mainLogo.png')} />
       <StatusBar style="auto" /> 
+      <View>
+        <Button title="WORKOUT" color="white" />
+        <Button title="CALORIES" color="white" />
+      </View>
     </View>
   );
 }
