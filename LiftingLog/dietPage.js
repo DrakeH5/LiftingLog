113c15fa3aef6b63f6c7188124de7e4fe6890f12
DrakeH5 <68834232+DrakeH5@users.noreply.github.com>
2023-01-27 17:37:10 -0600

    display diet history

--- LiftingLog/dietPage.js
+++ LiftingLog/dietPage.js
@@ -2,9 +2,34 @@ import { Text, View } from 'react-native';
 
 export default function Homepage() {
 
+    const mainStyle = {
+        backgroundColor: "rgb(50,50,50)", 
+        height: "100%", 
+        zIndex: -1,
+        justifyContent: "center",
+        alignItems: "center",
+    }
+
+    const generalTextStyle = {
+        color: "white",
+        fontSize: "30%",
+    }
+
+    var dietHistoryData = ["cookie", "Apple", "steak"]
+
+    var dietHistoryComponents = []
+
+    for(var i=0; i<dietHistoryData.length; i++){
+        dietHistoryComponents.push(
+            <View style={{borderColor: "grey", borderWidth: 2, margin: "3%"}}>
+                <Text style={generalTextStyle}>{dietHistoryData[i]}</Text>
+            </View>
+        )
+    }
+
     return (
-        <View style={{backgroundColor: "rgb(50,50,50)", height: "100%", zIndex: -1}}>
-            <Text>Diet</Text>
+        <View style={mainStyle}>
+            {dietHistoryComponents}
         </View>
     )
 }
