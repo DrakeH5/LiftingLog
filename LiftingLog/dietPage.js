727cfba7f23ef650145e2c27593351db0f23bfaf
DrakeH5 <68834232+DrakeH5@users.noreply.github.com>
2023-01-28 23:04:16 -0600

    add to diet history

--- LiftingLog/dietPage.js
+++ LiftingLog/dietPage.js
@@ -1,4 +1,5 @@
-import { Text, View } from 'react-native';
+import { Text, View, Modal, Button, TextInput } from 'react-native';
+import { useState } from 'react';
 
 export default function Homepage() {
 
@@ -15,21 +16,62 @@ export default function Homepage() {
         fontSize: "30%",
     }
 
-    var dietHistoryData = ["cookie", "Apple", "steak"]
+    const popupStyle = {
+        position: "relative", 
+        top: "50%", 
+        backgroundColor: "white", 
+        justifyContent: "center", 
+        alignItems: "center", 
+        margin: "10%"
+    }
 
-    var dietHistoryComponents = []
+    var dietHistoryData = ["Apple / 200"]
+
+    var dietHistoryComponentsFromServerData = []
 
     for(var i=0; i<dietHistoryData.length; i++){
-        dietHistoryComponents.push(
-            <View style={{borderColor: "grey", borderWidth: 2, margin: "3%"}}>
+        dietHistoryComponentsFromServerData.push(
+            <View id={i} style={{borderColor: "grey", borderWidth: 2, margin: "3%"}}>
                 <Text style={generalTextStyle}>{dietHistoryData[i]}</Text>
             </View>
         )
     }
 
+    var [dietHistoryComponents, setDietHistoryComponents] = useState(dietHistoryComponentsFromServerData)
+    
+
+    function addToHistory(name, cals){
+        setDietHistoryComponents((prevComponents) => [
+            ...prevComponents,
+            <View style={{borderColor: "grey", borderWidth: 2, margin: "3%"}}>
+                <Text style={generalTextStyle}>{name} / {cals}</Text>
+            </View>
+        ]);
+    }
+
+
+    const [foodNameInput, setFoodNameInput] = useState()
+    const [foodCalsInput, setFoodCalsInput] = useState()
+    function addFood(){
+        addToHistory(foodNameInput, foodCalsInput)
+        setPopUpVis(false)
+    }
+
+    const [popUpVis, setPopUpVis] = useState(false)
+
+
     return (
         <View style={mainStyle}>
             {dietHistoryComponents}
+            <Text style={{fontSize: "100%", color: "red", position: "absolute",  bottom: "1%", right: "5%"}} onPress={() => setPopUpVis(true)} >+</Text>
+            <Modal transparent={true} visible={popUpVis}>
+                <View style={popupStyle}>
+                    <TextInput onChangeText={text => setFoodNameInput(text)} placeholder="Food Name" style={{padding: 20}} id="foodName"></TextInput>
+                    <TextInput onChangeText={text => setFoodCalsInput(text)} placeholder="Est. Cals"></TextInput>
+                    <Button title="ADD" onPress={addFood}></Button>
+                    <Button title="X" onPress={() => {setPopUpVis(false)}}></Button>
+                </View>
+            </Modal>
         </View>
     )
 }
