dd528a824bbb98291b9aef4e26805802154a4a89
DrakeH5 <68834232+DrakeH5@users.noreply.github.com>
2023-02-11 14:27:48 -0600

    initialized database

--- server/server.js
+++ server/server.js
@@ -1,8 +1,27 @@
 const express = require("express");
 const app = express();
+const Datastore = require("nedb");
+
+const database = new Datastore("users.db");
+database.loadDatabase();
 
 app.get("/", (req, res) => {
   res.json("Hello world");
 });
+
+app.get("/getUsers", (req, res) => {
+  database.find({},(err, data) => {
+    if(err){
+      res.end(); 
+      return;
+    }
+    res.json(data);
+  });
+});
+
+/*app.get("/createAccount", (req, res) => {
+  const data = {"userName": req, "password": req., "workoutHistory": {}, "dietHistory": {}};
+  database.insert(data);
+})*/
 
 app.listen(5000, () => { console.log("Server started on port 5000") })
