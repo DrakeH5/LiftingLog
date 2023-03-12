6f2c94b4479e11fbc1474d70569c211f7477d740
DrakeH5 <68834232+DrakeH5@users.noreply.github.com>
2023-03-12 14:25:17 -0500

    show total protein for current day

--- LiftingLog/totalCalsForCurrentDay.js
+++ LiftingLog/totalCalsForCurrentDay.js
@@ -1,20 +1,37 @@
-import { Text } from 'react-native';
+import { Text, View } from 'react-native';
 
 export default function TotalCalsForCurrentDay({data}) {
 
-    const style = {
+    const Mainstyle = {
+        position: "relative",
+        top: "-15%",
+    }
+    const Calstyle = {
         color: "gold",
         fontSize: "35%",
+
+    }
+    const Protienstyle = {
+        color: "red",
+        fontSize: "25%",
         position: "relative",
-        top: "-15%"
+        top: "-15%",
+        textAlign: "center"
     }
 
     var totalCals = 0;
     for(var i=0; i<data.length; i++){
         totalCals+=data[i][1]*1
     }
+    var totalProtien = 0;
+    for(var i=0; i<data.length; i++){
+        totalProtien+=data[i][3]*1
+    }
     return(
-        <Text style={style}>Calories: {totalCals}</Text>
+        <View style={Mainstyle}>
+            <Text style={Calstyle}>Calories: {totalCals} {"\n"}</Text> 
+            <Text style={Protienstyle}>Protein: {totalProtien}</Text>
+        </View>
     )
 }
 
