0185d1a8ff7015a0f0b52dceb5d9f97da8754189
DrakeH5 <68834232+DrakeH5@users.noreply.github.com>
2023-03-12 13:32:36 -0500

    each meal displayed

--- LiftingLog/dateSpecificData.js
+++ LiftingLog/dateSpecificData.js
@@ -18,7 +18,7 @@ export default function DateSpecificData({date, data}) {
         color: "lightblue",
         fontSize: "40%",
         position: "relative",
-        top: "-27%",
+        top: "-25%",
         textAlign: "center"
     }
       const totalMacroInfoStyle = {
@@ -29,6 +29,12 @@ export default function DateSpecificData({date, data}) {
         textAlign: "left",
         width: "90%",
       }
+      const eachMealStyle = {
+        position: "absolute",
+        width: "100%",
+        top: "45%"
+      }
+
 
     var totalCals = 0;
     for(var i=0; i<data.length; i++){
@@ -47,6 +53,25 @@ export default function DateSpecificData({date, data}) {
       totalFat+=data[i][4]*1
     }
 
+    var eachMeal = [];
+    for(var i=0; i<data.length; i++){
+      eachMeal.push(
+        <View>
+          <Text style={{backgroundColor: "lightblue", width: "100%", textAlign: "center"}}>Food {i+1}</Text>
+          <Text style={{color: "white", width: "100%", textAlign: "center"}}>
+            <Text>{data[i][0]} </Text>
+            <Text> {data[i][1]} calories</Text>
+          </Text>
+          <Text style={{color: "white", width: "100%", textAlign: "center"}}>
+            <Text style={{color: "red"}}>{data[i][3]} Protein</Text>
+            <Text style={{color: "lightgreen"}}> {data[i][2]} Carbs</Text> 
+            <Text style={{color: "gold"}}> {data[i][4]} Fat</Text> 
+            {"\n"}
+          </Text>
+        </View>
+      )
+    }
+
     const widthAndHeight = 250
     const series = [123, 321, 123, 789, 537]
     const sliceColor = ['#F44336','#2196F3','#FFEB3B', '#4CAF50', '#FF9800']
@@ -61,9 +86,11 @@ export default function DateSpecificData({date, data}) {
           <Text style={totalMacroInfoStyle}>
             <Text style={{color: "red"}}>Total Protein: {totalProtein}</Text> {"\n"}
             <Text style={{color: "lightgreen"}}>Total Carbs: {totalCarbs}</Text> {"\n"}
-            <Text style={{color: "yellow"}}>Total Fat: {totalFat}</Text> 
+            <Text style={{color: "gold"}}>Total Fat: {totalFat}</Text> 
           </Text>
-          
+          <View style={eachMealStyle}>
+            {eachMeal}
+          </View>
           <Text>{data}</Text>
         </View>
       )
